2e74b97cf99038737d73a602bbb830af74cee83c
stephmen <stephmen@gmail.com>
2020-06-15 20:47:02 -0400

    removing unnecessary code

--- index.js
+++ index.js
@@ -45,44 +45,44 @@ app.ws('/video-stream', (ws, req) => {
   });
 });
 
-app.post('/receive', function (req, res) {
-  console.log("The Image Was Triggered")
-  var body = '';
-  var filePath = __dirname + '/Images/canvas.jpg';
-  console.log(filePath)
-  req.on('data', function (data) {
-    body += data;
-  });
-  // When whole image uploaded complete.
-  req.on('end', function () {
-    // Get rid of the image header as we only need the data parts after it.
-    var data = body.replace(/^data:image\/\w+;base64,/, "");
-    // Create a buffer and set its encoding to base64
-    var buf = new Buffer(data, 'base64');
-    // Write
-    fs.writeFile(filePath, buf, function (err) {
-      if (err) throw err
-      // Respond to client that the canvas image is saved.
-      var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-      var visualRecognition = new VisualRecognitionV3({
-        version: '2018-03-19',
-        iam_apikey: '8-Rj9GT4s1zsvI3LDGvvKtuRulW86EmU2eSArET5QTRf'
-      });
-      var images_file = fs.createReadStream('./Images/canvas.jpg');
-      var params = {
-        images_file: images_file,
-      };
-      visualRecognition.classify(params, function (err, response) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.stringify(response, null, 2))
-        }
-        res.send(JSON.stringify(response, null, 2));
-      });
-    });
-  });
-});
+// app.post('/receive', function (req, res) {
+//   console.log("The Image Was Triggered")
+//   var body = '';
+//   var filePath = __dirname + '/Images/canvas.jpg';
+//   console.log(filePath)
+//   req.on('data', function (data) {
+//     body += data;
+//   });
+//   // When whole image uploaded complete.
+//   req.on('end', function () {
+//     // Get rid of the image header as we only need the data parts after it.
+//     var data = body.replace(/^data:image\/\w+;base64,/, "");
+//     // Create a buffer and set its encoding to base64
+//     var buf = new Buffer(data, 'base64');
+//     // Write
+//     fs.writeFile(filePath, buf, function (err) {
+//       if (err) throw err
+//       // Respond to client that the canvas image is saved.
+//       var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+//       var visualRecognition = new VisualRecognitionV3({
+//         version: '2018-03-19',
+//         iam_apikey: '8-Rj9GT4s1zsvI3LDGvvKtuRulW86EmU2eSArET5QTRf'
+//       });
+//       var images_file = fs.createReadStream('./Images/canvas.jpg');
+//       var params = {
+//         images_file: images_file,
+//       };
+//       visualRecognition.classify(params, function (err, response) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(JSON.stringify(response, null, 2))
+//         }
+//         res.send(JSON.stringify(response, null, 2));
+//       });
+//     });
+//   });
+// });
 //////////////////////////////////////////////////////////
 /////////////////END///////////////////////////////////////
 app.use((err, req, res, next) => {
